1a2aea76dab9c9bf3ebd8e03496ffa0d86fad429
Suraj Aglave <aglavesuraj96@gmail.com>
2026-02-11 19:40:32 +0530

    .

---
+++ Add&Delete_Columns.sql
@@ -0,0 +1,9 @@
+-- Add a new column called email to the persons table.
+
+ALTER TABLE persons 
+ADD email VARCHAR(50) NOT NULL
+
+-- Remove the column phone from the persons table.
+
+ALTER TABLE persons
+DROP  COLUMN phone
